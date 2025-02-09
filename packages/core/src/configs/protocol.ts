3e5fd8d85ae6421a2ef8a9957b1350151b44719c
Nctdt <137032445@qq.com>
2025-02-09 17:28:26 +0800

    refactor: ♻️ protocol导出方式改成esm并加上jsdoc注释

--- packages/core/src/configs/protocol.ts
+++ packages/core/src/configs/protocol.ts
@@ -1,28 +1,35 @@
+/** 通用协议常量，定义了跨不同工具和框架交互的协议类型。*/
 const globalProtocol = {
-  ENTRY_FILE: "ENTRY_FILE", // 入口文件配置，如：入口文件引入全局 less、scss
+  /** 入口文件配置，例如引入全局的less、scss文件。 */
+  ENTRY_FILE: "ENTRY_FILE",
+  /** 更改文件导出的内容。 */
   UPDATE_EXPORT_CONTENT_PROTOCOL: "UPDATE_EXPORT_CONTENT_PROTOCOL",
+  /** 向文件插入import语句。 */
   INSERT_IMPORT_PROTOCOL: "INSERT_IMPORT_PROTOCOL",
+  /** 替换文件中对应slot的内容。 */
   SLOT_CONTENT_PROTOCOL: "SLOT_CONTENT_PROTOCOL",
-};
+} as const;
 
-// 插件对框架的协议
+/** 插件对框架的协议，扩展了通用协议，并添加处理样式的协议。 */
 const pluginToTemplateProtocol = {
   ...globalProtocol,
+  /** 处理样式协议，如：less、scss。 */
   PROCESS_STYLE_PLUGIN: "PROCESS_STYLE_PLUGIN",
-};
+} as const;
 
-// 插件对构建工具的协议
+/** 插件对构建工具的协议 */
 const pluginToBuildToolProtocol = {
   ...globalProtocol,
-};
+} as const;
 
-// 框架对构建工具的协议
+/** 框架对构建工具的协议 */
 const templateToBuildToolProtocol = {
   ...globalProtocol,
-  ADD_CONFIG: "ADD_CONFIG", //根据框架，不同的打包工具需要不同的插件，有些是都需要用的，有些是框架独有的
-};
+  /** 根据框架，不同的打包工具需要不同的插件，有些是都需要用的，有些是框架独有的 */
+  ADD_CONFIG: "ADD_CONFIG",
+} as const;
 
-module.exports = {
+export {
   globalProtocol,
   pluginToTemplateProtocol,
   pluginToBuildToolProtocol,
