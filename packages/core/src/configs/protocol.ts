244fb190fbbc68829974ab55cadca310c9da4950
Nctdt <137032445@qq.com>
2025-02-12 16:59:27 +0800

    docs: 📚 修改协议注释-2

--- packages/core/src/configs/protocol.ts
+++ packages/core/src/configs/protocol.ts
@@ -1,12 +1,12 @@
 /** 通用协议常量，定义了跨不同工具和框架交互的协议类型。*/
 const globalProtocol = {
-  /** 入口文件配置，例如引入全局的less、scss文件。 */
+  /** 入口文件配置，例如引入全局的 less、scss 文件。 */
   ENTRY_FILE: "ENTRY_FILE",
   /** 更改文件导出的内容。 */
   UPDATE_EXPORT_CONTENT_PROTOCOL: "UPDATE_EXPORT_CONTENT_PROTOCOL",
   /** 向文件插入import语句。 */
   INSERT_IMPORT_PROTOCOL: "INSERT_IMPORT_PROTOCOL",
-  /** 替换文件中对应slot的内容。 */
+  /** 对目标文件的指定插槽进行内容注入。 */
   SLOT_CONTENT_PROTOCOL: "SLOT_CONTENT_PROTOCOL",
 } as const;
 
@@ -25,7 +25,7 @@ const pluginToBuildToolProtocol = {
 /** 框架对构建工具的协议 */
 const templateToBuildToolProtocol = {
   ...globalProtocol,
-  /** 根据框架，不同的打包工具需要不同的插件，有些是都需要用的，有些是框架独有的 */
+  /** 自定义添加对应构建工具的配置 */
   ADD_CONFIG: "ADD_CONFIG",
 } as const;
 
